559324cdf36ef53355a0b39ebb18e85893ed2a6c
alcmene00 <alkminis@csd.auth.gr>
2022-02-17 19:44:35 +0200

    extract keywords

--- code/keywords.py
+++ code/keywords.py
@@ -8,8 +8,8 @@ from string import punctuation
 from collections import Counter
 import unicodedata as ud
 
-
 dataPath = '../data/Proceedings_500.csv'
+
 
 # start = time.time()
 # df = pd.read_csv(dataPath)
@@ -34,20 +34,21 @@ def remove_stopwords(df, stop_words):
         stop_words : list of stopwords
 
     """
-    
+
     number_of_rows = df.shape[0]
 
     # set header of new csv file
-    header = ['member_name', 'sitting_date', 'parliamentary_period', 'parliamentary_session', 'political_party', 'government', 'roles', 'member_gender', 'speech']
-    
+    header = ['member_name', 'sitting_date', 'parliamentary_period', 'parliamentary_session', 'political_party',
+              'government', 'roles', 'member_gender', 'speech']
+
     data = []
 
     for s in range(number_of_rows):
-        
+
         # create a list with all the elements in
         # column of a row of the csv file
-        data_row = []   
-        
+        data_row = []
+
         data_row.append(df['member_name'][s])
         data_row.append(df['sitting_date'][s])
         data_row.append(df['parliamentary_period'][s])
@@ -75,59 +76,46 @@ def remove_stopwords(df, stop_words):
         writer.writerows(data)
 
 
-def extract_keywords(nlp, sequence, stop_words, special_tags: list = None):
-    """ Takes a Spacy core language model,
-    string sequence of text and optional
-    list of special tags as arguments.
-
-    If any of the words in the string are
-    in the list of special tags they are immediately
-    added to the result.
-
-    Arguments:
-        sequence {str} -- string sequence to have keywords extracted from
-
-    Keyword Arguments:
-        tags {list} --  list of tags to be automatically added (default: {None})
-
-    Returns:
-        {list} -- list of the unique keywords extracted from a string
+def top_k_keywords(speeches, nlp, k):
     """
-    result = []
+    Args:
+        speeches: a list that contains speeches
+        nlp: spaCy's greek tokenizer
+        k: the number of most important keywords that should be returned
 
-    # custom list of part of speech tags we are interested in
-    # we are interested in proper nouns, nouns, and adjectives
-    # edit this list of POS tags according to your needs.
+    Returns: top-k keywords from specific speeches.
+    """
+
+    total_keywords = {}
+    for speech in speeches:
+        keywords = extract_keywords(nlp, speech)
+        for key in total_keywords:
+            if key in keywords:
+                total_keywords[key] = total_keywords[key] + keywords[key]
+        total_keywords = {**keywords, **total_keywords}
+
+    total_keywords = sorted(total_keywords, key=total_keywords.get, reverse=True)[:k]
+    return total_keywords
+
+
+def extract_keywords(nlp, speech):
+    """
+     Args:
+        speech: a speech
+        nlp: spaCy's greek tokenizer
+
+    Returns: keywords from a specific speech and how many times each word appears.
+    """
+
     pos_tag = ['PROPN', 'NOUN', 'ADJ']
-
-    # create a spacy doc object by calling the nlp object on the input sequence
-    doc = nlp(sequence.lower())
-
-    # if special tags are given and exist in the input sequence
-    # add them to results by default
-    if special_tags:
-        tags = [tag.lower() for tag in special_tags]
-        for token in doc:
-            if token.text in tags:
-                result.append(token.text)
-
-    for chunk in doc.noun_chunks:
-        final_chunk = ""
-        
-        for token in chunk:
-            if token.lemma_ in stop_words or token.text in punctuation:  # REMOVE STOPWORDS AND PUNCTUATION SYMBOLS
-                continue
-            if token.pos_ in pos_tag:
-                final_chunk = final_chunk + token.lemma_ + " "
-        if final_chunk:
-            result.append(final_chunk.strip())
+    result = []
+    doc = nlp(speech.lower())
 
     for token in doc:
-        if token.lemma_ in stop_words or token.text in punctuation:  # REMOVE STOPWORDS AND PUNCTUATION SYMBOLS
-            continue
         if token.pos_ in pos_tag:
             result.append(token.lemma_)
     dict_counter = dict(Counter(list(result)))
+
     return dict_counter
 
 
@@ -136,20 +124,12 @@ def main():
     nlp = spacy.load("el_core_news_sm")
     speeches = []
 
-    df = pd.read_csv(dataPath)
-
-    stop_words = nlp.Defaults.stop_words
-    stop_words.update({"κ.", "κύριος", "κυρία", "λόγο"})
-
-    remove_stopwords(df, stop_words)
-
     df = pd.read_csv('../data/Proceedings_Processed.csv')
     number_of_rows = df.shape[0]
-    
 
     choice = input('Εύρεση keywords, α)Ανα ομιλία, β)Ανά βουλευτή, γ)Ανα κόμμα  ')
     if choice == 'α':
-        number_of_speech = int(input('Δώσε αριθμό ομιλίας, μπορεί να είναι απο το 0-' + str(number_of_rows-1) + ' '))
+        number_of_speech = int(input('Δώσε αριθμό ομιλίας, μπορεί να είναι απο το 0-' + str(number_of_rows - 1) + ' '))
         if 0 <= number_of_speech < number_of_rows:
             speeches.append(df['speech'][number_of_speech])
 
@@ -170,21 +150,15 @@ def main():
             if df['political_party'][i] == name:
                 speeches.append(df['speech'][i])
 
-    print(len(speeches))
-    total_keywords = {}
-    for speech in speeches:
-        # nlp.max_length = len(speech) + 100
-        keywords = extract_keywords(nlp, speech, stop_words)
-        for key in total_keywords:
-            if key in keywords:
-                total_keywords[key] = total_keywords[key] + keywords[key]
-        total_keywords = {**keywords, **total_keywords}
+    print("Βρέθηκαν",  len(speeches), "ομιλίες")
     if len(speeches) == 0:
         print("Δεν βρέθηκαν ομιλίες")
-    else:
-        total_keywords = sorted(total_keywords, key=total_keywords.get, reverse=True)[:15]
-        print(total_keywords)
+        return
+
+    keywords = top_k_keywords(speeches, nlp, 15)
+    print(keywords)
     end = time.time()
-    print("Time", end-start)
+    print(" Time: " + f"{round(end-start, 2)} sec.\n")
+
 
 main()
