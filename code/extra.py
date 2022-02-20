d5fa4106cc5615d8f51d41b1e75ab7c65fedfa58
melinaz@live.com <melinaz@live.com>
2022-02-20 21:11:15 +0200

    find lsi for each year in a range of years

--- code/extra.py
+++ code/extra.py
@@ -1,51 +1,68 @@
 
+import time
 import pandas as pd
-import keywords
-from pprint import pprint
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from pywebio.output import put_text, put_markdown, put_html
+from pywebio.input import input, radio
 
 # data_path = '../data/Proceedings_Processed.csv'
-# data_path = '../data/Proceedings_1989_2020_Processed.csv'
+data_path = '../data/Proceedings_1989_2020_Processed.csv'
 
+def similarities(A, B):
+    A = A.lower()
+    B = B.lower()
+    list1 = A.split(" ")
+    list2 = B.split(" ")
+    return len(list(set(list1) & set(list2)))
 
 def main(data_path):
     df = pd.read_csv(data_path)
     number_of_rows = df.shape[0]
+
+    put_markdown = ('# **Επιλέξτε το διάστημα ετών που θέλετε να δείτε την μεταβολή των σημαντικότερων θεματικών.**')
+    start_year = int(input('Από: '))
+    end_year = int(input('Μέχρι: '))
+
+    lsi = []
+
+    speeches = []         
+
+    start = time.time()
+    for year in range(start_year, end_year + 1):
+        
+        for i in range(number_of_rows):
+
+            if str(year) in str(df['sitting_date'][i]):
+                speeches.append(str(df['speech'][i]))
+
+        if speeches:
+            vect = TfidfVectorizer()
+            tfidf_matrix = vect.fit_transform(speeches)
+
+            lsa_model = TruncatedSVD(n_components=1, n_iter=7)
+
+            lsa_top = lsa_model.fit_transform(tfidf_matrix)
+            l = lsa_top[0]
+
+            terms = vect.get_feature_names_out()
+
+            for i, component in enumerate(lsa_model.components_):
+                zipped = zip(terms, component)
+                terms_key = sorted(zipped, key = lambda t: t[1], reverse=True)[:10]
+                terms_list = list(dict(terms_key).keys())
+                lsi.append(terms_list)
+                put_text(year)
+                put_text(terms_list)
+
+                # for i, topic in enumerate(l):
+                #     put_text("Θεματική Περιοχή ", i ," : ", topic * 100)
+                #     put_text(lsa_model.components_)
+                #     put_text("\n")
+
+        speeches.clear()
+
     
-    members = []
-    members_names = []
+    # put_text(lsi)
 
-    member_name = input('Δώσε όνομα βουλευτή ')
-    speeches, number_of_speeches = keywords.find_speeches(df, member_name, 'member_name', number_of_rows)
-
-    dates = []
-
-
-    for s in range(number_of_rows):
-        # create a list with all the elements in
-        # column of a row of the csv file
-
-        if df['member_name'][s] == member_name:
-            dates.append( df['sitting_date'][s])
-
-
-    earliest = min(dates[0])
-    latest = max(dates[0])
-
-    min_index = dates[0].index(earliest)
-    max_index = dates[0].index(latest)
-
-    # print(keywords.extract_only_keywords(df['speech'][min_index]))
-    min_speech_keywords = keywords.extract_only_keywords(df['speech'][min_index])
-    max_speech_keywords = keywords.extract_only_keywords(df['speech'][max_index])
-
-    # print(min_index, max_index)
-    # print(min_speech_keywords)
-
-    # res = len(set(min_speech_keywords) & set(max_speech_keywords)) / float(len(set(min_speech_keywords) | set(max_speech_keywords))) * 100
-    # TODO correct calculation
-    res = 200.0 * len(set(min_speech_keywords) & set(max_speech_keywords)) / (len(min_speech_keywords) + len(max_speech_keywords))
-    # print(set(min_speech_keywords) & set(max_speech_keywords))
-    # # printing result
-    # print(len(set(min_speech_keywords) & set(max_speech_keywords)))
-
-# main(data_path)
+main(data_path)
