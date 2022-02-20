5e605eb1ce3e7476a32d77f6a91d2fb673136537
melinaz@live.com <melinaz@live.com>
2022-02-20 08:33:30 +0200

    update web interface (lsi option)

--- code/web_app.py
+++ code/web_app.py
@@ -10,11 +10,14 @@ dataPath = '../data/Proceedings_1989_2020_Processed.csv'
 def webapp_main():
     option_keywords = 'Σημαντικότερες λέξεις κλειδιά'
     option_tf_idf = 'Τop-k ζεύγη με τον υψηλότερο βαθμό ομοιότητας'
-    mode = radio("Αναζήτηση ", options=[option_keywords, option_tf_idf , 'lsa', '?'])
+    option_lsi = 'Σημαντικότερες θεματικές περιοχές'
+    mode = radio("Αναζήτηση ", options=[option_keywords, option_tf_idf , option_lsi, '?'])
     if mode == option_keywords:
         keywords.main(dataPath)
     if mode == option_tf_idf:
         tf_idf.main(dataPath)
+    if mode == option_lsi:
+        lsi.main(dataPath)
 
 
 if __name__ == '__main__':
